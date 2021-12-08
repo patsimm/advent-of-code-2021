83d9eb2ddc94f9fffb4748f4b8ef529ea8633f64
Patrick Simmelbauer <patrick.simmelbauer@gmail.com>
2021-12-08 20:59:24 +0100

    find mapping for day 8

---
+++ day-8.ts
@@ -0,0 +1,165 @@
+import getStdin from "get-stdin";
+
+const rawInput = await getStdin();
+
+const lines = rawInput
+  .trim()
+  .split("\n")
+  .map((line) => parseLine(line.trim()));
+
+function parseLine(line: string) {
+  const [input, output] = line
+    .trim()
+    .split("|")
+    .map((vals) =>
+      vals
+        .trim()
+        .split(" ")
+        .map((val) => val.split("") as Key[])
+    );
+  return { input, output };
+}
+
+function isOne(value: Key[]) {
+  return value.length === 2;
+}
+
+function isFour(value: Key[]) {
+  return value.length === 4;
+}
+
+function isSeven(value: Key[]) {
+  return value.length === 3;
+}
+
+function isEight(value: Key[]) {
+  return value.length === 7;
+}
+
+function uniqueNumbersAmount(values) {
+  return values.filter(
+    (val) => isOne(val) || isFour(val) || isSeven(val) || isEight(val)
+  ).length;
+}
+
+console.log(
+  lines
+    .map((line) => uniqueNumbersAmount(line.output))
+    .reduce((prev, curr) => prev + curr, 0)
+);
+
+const lengths = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6];
+
+const keys = ["a", "b", "c", "d", "e", "f", "g"] as const;
+const segmentTypes = ["A", "B", "C", "D", "E", "F", "G"] as const;
+
+type Key = typeof keys[number];
+type SegmentType = typeof segmentTypes[number];
+
+function findMapping(line: ReturnType<typeof parseLine>) {
+  const segments: Record<SegmentType, Key[]> = segmentTypes.reduce(
+    (prev, curr) => {
+      prev[curr] = [...keys];
+      return prev;
+    },
+    {} as Record<SegmentType, Key[]>
+  );
+
+  function restrictToVal(segType: SegmentType, val: Key[]) {
+    segments[segType] = segments[segType].filter((key) => val.includes(key));
+  }
+
+  function excludeVal(segType: SegmentType, val: Key[]) {
+    segments[segType] = segments[segType].filter((key) => !val.includes(key));
+  }
+
+  function excludeFromOthers(segTypes: SegmentType[], val: Key[]) {
+    segmentTypes
+      .filter((t) => !segTypes.includes(t))
+      .forEach((t) => excludeVal(t, val));
+  }
+
+  line.input.forEach((val) => {
+    if (isOne(val)) {
+      restrictToVal("C", val);
+      restrictToVal("F", val);
+    }
+    if (isFour(val)) {
+      restrictToVal("B", val);
+      restrictToVal("C", val);
+      restrictToVal("D", val);
+      restrictToVal("F", val);
+    }
+    if (isSeven(val)) {
+      restrictToVal("A", val);
+      restrictToVal("C", val);
+      restrictToVal("F", val);
+    }
+  });
+
+  const inputValuesWithLenghtFive = line.input.filter(
+    (val) => val.length === 5
+  );
+  if (inputValuesWithLenghtFive.length === 3) {
+    const keyCounts = inputValuesWithLenghtFive.reduce((prev, curr) => {
+      curr.forEach((key) => (prev[key] = prev[key] ? prev[key] + 1 : 1));
+      return prev;
+    }, {} as Partial<Record<Key, number>>);
+    const restrictTo = keys.filter((key) => keyCounts[key] === 1);
+    restrictToVal("B", restrictTo);
+    restrictToVal("E", restrictTo);
+  }
+
+  const inputValuesWithLenghtSix = line.input.filter((val) => val.length === 6);
+  if (inputValuesWithLenghtSix.length === 3) {
+    const keyCounts = inputValuesWithLenghtSix.reduce((prev, curr) => {
+      curr.forEach((key) => (prev[key] = prev[key] ? prev[key] + 1 : 1));
+      return prev;
+    }, {} as Partial<Record<Key, number>>);
+    const excludeFromC = keys.filter((key) => keyCounts[key] === 3);
+    restrictToVal("C", excludeFromC);
+  }
+
+  function finished() {
+    return !Object.values(segments).some((seg) => seg.length !== 1);
+  }
+
+  while (!finished()) {
+    const typesWithOnlyOnePossibleValue = segmentTypes.filter(
+      (segType) => segments[segType].length === 1
+    );
+    typesWithOnlyOnePossibleValue.forEach((segType) =>
+      excludeFromOthers([segType], segments[segType])
+    );
+
+    const typesWithOnlyTwoPossibleValues = segmentTypes.filter(
+      (segType) => segments[segType].length === 2
+    );
+    const countTypesWithOnlyTwoPossibleValues = typesWithOnlyTwoPossibleValues
+      .map((segType) => segments[segType].join(""))
+      .reduce((prev, curr) => {
+        prev[curr] = prev[curr] ? prev[curr] + 1 : 1;
+        return prev;
+      }, {} as Partial<Record<string, number>>);
+
+    Object.keys(countTypesWithOnlyTwoPossibleValues)
+      .filter((keys) => countTypesWithOnlyTwoPossibleValues[keys] === 2)
+      .forEach((keys) =>
+        excludeFromOthers(
+          segmentTypes.filter((segType) => segments[segType].join("") === keys),
+          keys.split("") as Key[]
+        )
+      );
+  }
+
+  return {
+    A: segments.A[0],
+    B: segments.B[0],
+    C: segments.C[0],
+    E: segments.E[0],
+    F: segments.F[0],
+    G: segments.G[0],
+  };
+}
+
+lines.forEach((line) => console.log(findMapping(line)));
